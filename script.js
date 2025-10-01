81d1ff30a55ed2f1f25656cb9f552808f4eea533
CadetheCoder <cadehultz06@gmail.com>
2025-10-01 19:43:28 +0900

    Fixed breakpoint for project imgs

--- script.js
+++ script.js
@@ -4,8 +4,6 @@ nikeProject.addEventListener("click", ()=>{
     window.alert("Sorry! This website is currently under development. Please check back soon.")
 })
 
-
-
 window.addEventListener('load', () => {
     const heroContainer = document.querySelector(".hero-container");
     heroContainer.classList.add("loaded");
