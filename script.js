db461c0cad451eb1b7ed9803d17e149b633a223d
CadetheCoder <cadehultz06@gmail.com>
2025-10-01 17:39:55 +0900

    Added alert message to nike project

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
+const nikeProject = document.getElementById("nike-project");
+
+nikeProject.addEventListener("click", ()=>{
+    window.alert("Sorry! This website is currently under development. Please check back soon.")
+})
+
+
+
 window.addEventListener('load', () => {
     const heroContainer = document.querySelector(".hero-container");
     heroContainer.classList.add("loaded");
 })
+
